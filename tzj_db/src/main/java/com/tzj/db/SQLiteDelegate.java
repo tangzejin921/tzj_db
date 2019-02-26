a8cb1d3dff8aa2f365c2615fecb2fe8de88d765b
tangzejin921 <tangzejin921@qq.com>
2019-02-26 14:45:43 +0800

    修改让实现一库多表

--- tzj_db/src/main/java/com/tzj/db/SQLiteDelegate.java
+++ tzj_db/src/main/java/com/tzj/db/SQLiteDelegate.java
@@ -1,8 +1,12 @@
 package com.tzj.db;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by tzj on 2018/5/21.
@@ -26,6 +30,9 @@ public class SQLiteDelegate extends SQLiteOpenHelper {
     public void onConfigure(SQLiteDatabase db) {
         sqlite.initFields();
         super.onConfigure(db);
+        if (!isExist(db,sqlite.tabName())){
+            onCreate(db);
+        }
     }
 
     @Override
@@ -35,6 +42,29 @@ public class SQLiteDelegate extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         sqlite.onUpgrade(db,oldVersion,newVersion);
+    }
+
+    /**
+     * 是否第一次 或者 已经创建了表
+     */
+    private boolean isExist(SQLiteDatabase db,String tableName){
+        db.beginTransaction();
+        List<String> list = new ArrayList<>();
+        Cursor cursor = db.rawQuery("SELECT name FROM sqlite_master where type='table'",new String[]{});
+        while (cursor.moveToNext()){
+            String temp = cursor.getString(0);
+            list.add(temp);
+        }
+        list.remove("sqlite_sequence");
+        list.remove("android_metadata");
+        cursor.close();
+        db.endTransaction();
+        //第一次 或者 已经创建了表
+        if (list.size() == 0 || list.contains(tableName)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
 }
